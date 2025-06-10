2d3280550a0e68d3c51e8203e5c015f0c37ab025
samdoolin <sam.doolin@assimila.eu>
2025-06-10 18:01:18 +0100

    routes

--- utils/settings.py
+++ utils/settings.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 # this is a custom field in STAC collection metadata
 # which provides some default visualization parameters for the zarr datacube
 WPL_RENDER_KEY = "wpl:render"
@@ -10,3 +12,26 @@ POINT_OF_INTEREST_OPTS = {
 }
 
 CATALOG_URL = "https://s3.waw3-2.cloudferro.com/swift/v1/wpl-stac/stac/catalog.json"
+
+
+type RouteName = str
+type RouteURL = str
+type RouteParams = list[str]
+type RouteInfo = tuple[RouteURL, RouteParams]
+
+
+# URLs used to generate links between pages
+ROUTES: dict[RouteName, RouteInfo] = {
+    "index": ("/", []),
+    "indicators": ("/indicators", ["site-id"]),
+    "data": ("/data", ["site-id", "collection-id"]),
+    "collections": ("/collections", ["site-id"]),
+}
+
+# additional metadata key for STAC collection
+WPL_BIOME_KEY = "wpl:biome"
+
+type Biome = Literal["boreal", "temperate", "tropical"]
+
+
+BIOME_COLOUR: dict[Biome, str] = {"boreal": "teal", "temperate": "darkgoldenrod", "tropical": "green"}
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,13 @@
+import urllib.parse
+from typing import get_args
+
 import geoviews as gv
 import holoviews as hv
+import panel as pn
+import pystac
 from holoviews import streams
+
+from . import settings
 
 
 def attach_stream_to_map(steam: streams.Stream, dynamic_map: gv.DynamicMap) -> gv.Overlay:
@@ -27,3 +34,54 @@ def attach_stream_to_time_series(steam: streams.Stream, dynamic_map: hv.DynamicM
     event_source = hv.Scatter([])
     steam.source = event_source
     return event_source * dynamic_map
+
+
+def get_url(route_name: str, kwargs: dict | None = None) -> str:
+    """
+    Get the URL for a named route, which may expect query parameters.
+    """
+    if kwargs is None:
+        kwargs = {}
+    url, params = settings.ROUTES[route_name]
+    if len(params) == 0:
+        return url
+    qstring = urllib.parse.urlencode({k: kwargs[k] for k in params})
+    return f"{url}?{qstring}"
+
+
+@pn.cache
+def get_root_catalog() -> pystac.Catalog:
+    return pystac.read_file(settings.CATALOG_URL)  # type: ignore
+
+
+def catalog_hash(catalog: pystac.Catalog) -> bytes:
+    hash = catalog.id + " " + catalog.self_href
+    return hash.encode("utf-8")
+
+
+@pn.cache(hash_funcs={pystac.Catalog: catalog_hash})
+def get_sub_catalogs(catalog: pystac.Catalog) -> list[pystac.Catalog]:
+    return [child for child in catalog.get_children() if child.STAC_OBJECT_TYPE == pystac.STACObjectType.CATALOG]
+
+
+@pn.cache(hash_funcs={pystac.Catalog: catalog_hash})
+def get_collections(catalog: pystac.Catalog) -> list[pystac.Collection]:
+    return list(catalog.get_collections())
+
+
+def get_biome(site: pystac.Catalog) -> settings.Biome | None:
+    """Look for a `wpl:biome` key in catalog metadata"""
+    if settings.WPL_BIOME_KEY in site.extra_fields:
+        biome = site.extra_fields[settings.WPL_BIOME_KEY].lower()
+        if biome in get_args(settings.Biome.__value__):
+            return biome
+    return None
+
+def get_biome_colour(biome: str | None) -> str:
+    """
+    Get the colour associated with a biome.
+    """
+    try:
+        return settings.BIOME_COLOUR[biome]  # type: ignore
+    except KeyError:
+        return "grey"  # default to grey if biome not found
